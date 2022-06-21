6911cddc649ac026441362be953818b20f21ba3f
moribus-single <dvg37@tpu.ru>
2022-06-21 17:39:12 +0700

    feat: borrow and repay

--- contracts/AdapterVenus.sol
+++ contracts/AdapterVenus.sol
@@ -5,14 +5,19 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "./IVbep20.sol";
 import "./IUnitroller.sol";
 
+import "hardhat/console.sol";
+
 /**
  * @title Adapter for interacting with Venus Protocol
  * @author Galas' Danil
  * @dev Implementation of adapter for interaction with Venus Protocol
  */
 contract AdapterVenus {
-
+    /**
+     * @dev Address of the Unitroller.
+     */
     address private constant UNITROLLER = 0xfD36E2c2a6789Db23113685031d7F16329158384;
+    
     /**
      * @dev Lend some Token to the protocol, get vToken as guarantee of collateral
      * 
@@ -25,11 +30,14 @@ contract AdapterVenus {
         IERC20(Token).transferFrom(msg.sender, address(this), mintAmount);
         IERC20(Token).approve(vToken, mintAmount);
 
-        uint256 balanceBefore = IERC20(vToken).balanceOf(address(this));
+        // uint256 balanceBefore = IERC20(vToken).balanceOf(address(this));
         assert(IVBep20(vToken).mint(mintAmount) == 0);
-        uint256 balanceAfter = IERC20(vToken).balanceOf(address(this));
+        // uint256 balanceAfter = IERC20(vToken).balanceOf(address(this));
 
-        IERC20(vToken).transfer(msg.sender, balanceAfter - balanceBefore);
+        // IERC20(vToken).transfer(msg.sender, balanceAfter - balanceBefore);
+
+        (,uint256 liquidity,) = IUnitroller(UNITROLLER).getAccountLiquidity(address(this));
+        console.log(liquidity);
     }
 
     /**
@@ -40,21 +48,45 @@ contract AdapterVenus {
      * @param vToken Address of the wrapped token to get back 
      */
     function redeem(uint256 redeemTokens, address Token, address vToken) external {
-        IERC20(vToken).transferFrom(msg.sender, address(this), redeemTokens);
         IERC20(vToken).approve(vToken, redeemTokens);
-
         uint256 balanceBefore = IERC20(Token).balanceOf(address(this));
-        assert(IVBep20(vToken).redeem(redeemTokens) == 0);
+        uint256 code = IVBep20(vToken).redeem(redeemTokens);
+        console.log(code);
+        assert(code == 0);
         uint256 balanceAfter = IERC20(Token).balanceOf(address(this));
 
         IERC20(Token).transfer(msg.sender, balanceAfter - balanceBefore);
     }
 
+    /**
+     * @dev Borrow some tokens from the protocol
+     *
+     * @param borrowAmount Amount of the tokens to borrow
+     * @param vToken Address of the wrapped token to borrow
+     */
+    function borrow(uint256 borrowAmount, address vToken) external {
+        (,uint256 liquidity,) = IUnitroller(UNITROLLER).getAccountLiquidity(address(this));
+        require(liquidity > borrowAmount, "account's liquidity is too small");
+        
+        assert(IVBep20(vToken).borrow(borrowAmount) == 0);
+    }
+
+    function repayBorrow(uint256 repayAmount, address Token, address vToken) external {
+        IERC20(Token).approve(vToken, repayAmount);
+        assert(IVBep20(vToken).repayBorrow(repayAmount) == 0);
+    } 
+
+    /**
+     * @dev Repay your own borrow
+     *
+     * @param vTokens List of the vTokens to enter
+     */
+
     function enterMarkets(address[] calldata vTokens) external {
         uint[] memory codes = IUnitroller(UNITROLLER).enterMarkets(vTokens);
         
         for(uint i; i < codes.length; i++) {
-            assert(codes[i] == 0);
+            console.log("codes[",i,"]=", codes[i]);
         }
     } 
 }
--- test/venus-swapper-test.ts
+++ test/venus-swapper-test.ts
@@ -10,76 +10,117 @@ import { deployWithoutParams } from "./helpers"
 describe("Venus adapter", function () {
     let ADAPTER_name: string = config.AdapterVenus.name;
     
+    let vUSDT_addr: string = config.vTokens.vUSDT.address;
+    let vLTC_addr: string = config.vTokens.vLTC.address;
+
+    let LTC_addr: string = config.Tokens.LTC.address;
+    let USDT_addr: string = config.Tokens.USDT.address;
+
     let signers: SignerWithAddress[];
     let adapter: AdapterVenus;
+
+    let vUSDT: IVBep20;
+    let USDT: IERC20;
+
+    let vLTC: IVBep20;
+    let LTC: IERC20;
+
+    let holderLTC: SignerWithAddress;
+    let LTCBalance: BigNumber;
+
+    let holderUSDT: SignerWithAddress;
+    let USDTBalance: BigNumber;
 
     before(async function () {
         signers = await ethers.getSigners();
 
         // Deploying adapter
         adapter = <AdapterVenus>(await deployWithoutParams(ADAPTER_name));
+        
+        // Entering markets
+        await adapter.enterMarkets([vLTC_addr, vUSDT_addr]);
+
+        // Getting contract instances
+        vUSDT = <IVBep20>(await ethers.getContractAt("IVBep20", vUSDT_addr));
+        USDT = <IERC20>(await ethers.getContractAt("IERC20", USDT_addr));
+
+        vLTC = <IVBep20>(await ethers.getContractAt("IVBep20", vLTC_addr));
+        LTC = <IERC20>(await ethers.getContractAt("IERC20", LTC_addr));
+
+        // Impersonating USDT holder
+        const holderUSDTaddr = "0x85362633c6ab0bd39aaf73dd8586ac8e8e29c821";
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [holderUSDTaddr],
+        });
+        holderUSDT = await ethers.getSigner(holderUSDTaddr);
+
+        // Impersonating LTC holder
+        const holderLTCaddr = "0xF977814e90dA44bFA03b6295A0616a897441aceC";
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [holderLTCaddr],
+        });
+        holderLTC = await ethers.getSigner(holderLTCaddr);
+
+        // Sending 100 ETH to the LTC holder
+        signers = await ethers.getSigners();
+        await signers[5].sendTransaction({
+            to: holderLTCaddr,
+            value: ethers.utils.parseEther("100")
+        });
+
+        // Sending 100 ETH to the USDT holder
+        signers = await ethers.getSigners();
+        await signers[5].sendTransaction({
+            to: holderUSDTaddr,
+            value: ethers.utils.parseEther("100")
+        });
     });
 
-    describe("Lend USDT", function () {
-        let vUSDT_addr: string = config.vTokens.vUSDT.address;
-        let USDT_addr: string = config.Tokens.USDT.address;
+    it("Check balance of the USDT holder", async () => {
+        const balance = await USDT.balanceOf(holderUSDT.address);
+        expect(balance).eq(BigNumber.from("200000000000000000"));
 
-        let vUSDT: IVBep20;
-        let USDT: IERC20;
+        USDTBalance = balance;
+    });
 
-        let holderUSDT: SignerWithAddress;
-        let holderBalance: BigNumber;
+    it("Check balance of the LTC holder", async () => {
+        const balance = await LTC.balanceOf(holderLTC.address);
+        expect(balance).eq(BigNumber.from("122543000000000000000000"))
+        
+        LTCBalance = balance; 
+    });
 
-        before(async function () {
-            // Getting contract instances
-            vUSDT = <IVBep20>(await ethers.getContractAt("IVBep20", vUSDT_addr));
-            USDT = <IERC20>(await ethers.getContractAt("IERC20", USDT_addr));
+    it("Lend some USDT", async () => {
+        const mintAmount = USDTBalance.div(2);
 
-            // Impersonating USDT holder
-            const holderUSDTaddr = "0x85362633c6ab0bd39aaf73dd8586ac8e8e29c821";
-            await hre.network.provider.request({
-                method: "hardhat_impersonateAccount",
-                params: [holderUSDTaddr],
-            });
-            holderUSDT = await ethers.getSigner(holderUSDTaddr);
+        await USDT.connect(holderUSDT).approve(adapter.address, mintAmount);
+        await adapter.connect(holderUSDT).mint(mintAmount, USDT_addr, vUSDT_addr);
 
-            // Sending 100 ETH to the USDT holder
-            signers = await ethers.getSigners();
-            await signers[5].sendTransaction({
-                to: holderUSDTaddr,
-                value: ethers.utils.parseEther("100")
-            });
+        const balancevUSDT = await vUSDT.balanceOf(adapter.address);
+        expect(balancevUSDT).eq(BigNumber.from("460148851"));
+    }); 
 
-            // Entering markets from the adapter
-            await adapter.enterMarkets([vUSDT_addr]);
-        });
+    it("Borrow some tokens", async () => {
+        const borrowAmount = 100;
 
-        it("Check balance of the USDT holder", async () => {
-            const balance = await USDT.balanceOf(holderUSDT.address);
-            expect(balance).eq(BigNumber.from("200000000000000000"));
+        await adapter.borrow(borrowAmount, vLTC_addr);
+        expect(await LTC.balanceOf(adapter.address)).eq(borrowAmount); 
+    });
 
-            holderBalance = balance;
-        });
+    it("Repay borrow", async () => {
+        const repayAmount = 100;
 
-        it("Lend some USDT to the vUSDT market from USDT holder", async () => {
-            const mintAmount = holderBalance.div(2);
+        await adapter.repayBorrow(repayAmount, LTC_addr, vLTC_addr);
+        expect(await LTC.balanceOf(adapter.address)).eq(0); 
+    });
 
-            await USDT.connect(holderUSDT).approve(adapter.address, holderBalance);
-            await adapter.connect(holderUSDT).mint(mintAmount, USDT_addr, vUSDT_addr);
+    it("Redeem USDT", async () => {
+        const redeemAmount = BigNumber.from("460148851");
 
-            const balancevUSDT = await vUSDT.balanceOf(holderUSDT.address);
-            expect(balancevUSDT).eq(BigNumber.from("460148852"));
-        });
-
-        it("Redeem USDT back to holder", async () => {
-            const redeemAmount = BigNumber.from("460148852");
-
-            await vUSDT.connect(holderUSDT).approve(adapter.address, redeemAmount);
-            await adapter.connect(holderUSDT).redeem(redeemAmount, USDT_addr, vUSDT_addr);
-
-            expect(await USDT.balanceOf(holderUSDT.address)).above(holderBalance);
-        });
-
-
+        await adapter.connect(holderUSDT).redeem(redeemAmount, USDT_addr, vUSDT_addr);
+        expect(await USDT.balanceOf(holderUSDT.address)).above(USDTBalance);
     });
 });
+
